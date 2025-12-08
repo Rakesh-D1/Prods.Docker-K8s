7bf09a8eb3b3a4338c5d00f37a018ac5790e8bed
Rakesh-D1 <rs002sam@gmail.com>
2025-12-08 17:55:06 +0530

    +

---
+++ 4/39/simpleweb/notes.sh
@@ -0,0 +1,7 @@
+// Here the issue came ddue to the base level image is taken as Alpine
+// Where the mode.js image is not supported on the alpine tthahy we get on the second step 
+
+Step 2/3 : RUN npm install
+ ---> Running in ec3da303246f
+// /bin/sh: npm: not found
+//The command '/bin/sh -c npm install' returned a non-zero code: 127
