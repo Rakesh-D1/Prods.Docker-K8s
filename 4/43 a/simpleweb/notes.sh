d68ca3d4e20f937fbdfba83f37ea186e10b7274d
Rakesh-D1 <rs002sam@gmail.com>
2025-12-09 21:04:47 +0530

    +

--- 4/43 a/simpleweb/notes.sh
+++ 4/43 a/simpleweb/notes.sh
@@ -1,29 +1,12 @@
-// Give a tag name while creating the images 
-// Commd is doce
-// DOCKER_BUILDKIT=0 image build -t imagename . // here we are giving the tag name 
-// Post successfully creatiobn of the Docker image Sending build context to Docker daemon   5.12kB
-Step 1/4 : FROM node:14-alpine
- ---> 434215b487a3
-Step 2/4 : COPY ./ ./
- ---> Using cache
- ---> 4cddc9f08153
-Step 3/4 : RUN npm install
- ---> Using cache
- ---> 07761f56fe26
-Step 4/4 : CMD [ "npm", "start" ]
- ---> Using cache
- ---> 1d7370ba408d
-Successfully built 1d7370ba408d
-Successfully tagged b88devops/simpleweb:latest
+Here we change the port number in the index.js file and chkd the conatiner running with the give port
+ 
+Chnages in the index.js 
+app.listen(6000, () => {
+  console.log('Listening on port 6000');
+});
 
-// Now create a new container using the same node image
-// docker run b88devops/simpleweb:latest
+now run the command -  $ DOCKER_BUILDKIT=0 docker build -t b88devops/simpleweb .
 
-// Post creation of the container the localhost:8080 wont work now since the port configuration is not done
-// The applicaiton is running on container 8080 we need to connect the contatiner now we have to forward this to the host FS 
+Once the images is created now run the container
 
-docker run -p 8080(host forward port):8080 (container forward port) b88devops/simpleweb:latest
-
-docker cli will send the command to docker daemon to run & transport the port from container FS to host FS  
-
-now run the command -  docker run -p 8080:8080 b88devops/simpleweb:latest
+$ docker run -p 5000:6000 b88devops/simpleweb
