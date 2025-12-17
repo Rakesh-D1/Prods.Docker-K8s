2330cdcfb604e7d3e182523eee168d0aa95ffc0d
Rakesh-D1 <rs002sam@gmail.com>
2025-12-17 17:25:37 +0530

    +

---
+++ 5/53.DockerComposeFile-networking-buildkit/notes.sh
@@ -0,0 +1,15 @@
+using the buildkit we need to run the 
+
+student@DESKTOP-VI7VSG8 MINGW64 ~/Desktop/Prods.Docker-K8s/5/53.DockerComposeFile-networking-buildkit (main)
+$ DOCKER_BUILDKIT=0 docker-compose up
+time="2025-12-17T15:34:35+05:30" level=warning msg="C:\\Users\\student\\Desktop\\Prods.Docker-K8s\\5\\53.DockerComposeFile-networking-buildkit\\docker-compose.yml: the attribute `version` is obsolete, it will be ignored, please remove it to avoid potential confusion"
+[+] Running 7/10
+ - redis-server [⣿⣿⣿⣤⣿⣿⣀⣿⣿] 26.22MB / 55.36MB Pulling                                                                                               6.1s 
+   ✔ 0c16123e68fb Download complete                                                                                                                 0.5s 
+   ✔ 1d9a95b931a8 Download complete                                                                                                                 0.8s 
+   ✔ 2cedcff21aff Download complete                                                                                                                 0.4s 
+   - ae4ce04d0e1c Downloading     [===========================>                       ]  15.73MB/28.23MB                                            2.8s 
+   ✔ 60f52e32d520 Download complete                                                                                                                 0.8s 
+   ✔ 4f4fb700ef54 Download complete                                                                                                                 1.3s 
+   - 30e994012175 Downloading     [================>                                  ]  8.389MB/24.75MB                                            2.8s 
+   ✔ db91eab6df36 Download complet
---
+++ 5/56.automatic-container-restarts/c.unless-stopped/note.sh
@@ -0,0 +1,5 @@
+We have commneted the Process
+ app.get('/', (req, res) => {
+ // process.exit(1);  // Here in unless topped we have commendted the process exit
+
+  
