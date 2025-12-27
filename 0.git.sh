d6d8e9e86761a7fc1453bdf44edd2a9b3aa0af5d
wsl-docker-k8s <you@example.com>
2025-12-27 06:53:49 +0000

    adding code to docker K8s

---
+++ 0.git.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+git add .
+git commit -m "adding code to docker K8s"
+git push
--- 6/65-dockerfile.dev/a/frontend/note.sh
+++ 6/65-dockerfile.dev/a/frontend/note.sh
@@ -2,3 +2,7 @@ Here we have created a new Front end react project in Node app
 post building the npm will create the node_modules folder
 all the dependecies are added im the package.json folder
 post the npm install the package.lock file will get created to maintian the current state
+
+
+docker run -p 3004:3000 -v 
+docker run -p 3004:3000 -v /app/node_modules -v "$(pwd)":/app e0b1c36dd
