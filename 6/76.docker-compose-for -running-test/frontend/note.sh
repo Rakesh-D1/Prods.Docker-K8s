59fe180d9e3ad63e6eb89788f01534688c18a14a
wsl-docker-k8s <you@example.com>
2026-01-03 14:46:34 +0000

    +

--- 6/76.docker-compose-for -running-test/frontend/note.sh
+++ 6/76.docker-compose-for -running-test/frontend/note.sh
@@ -1,2 +1,23 @@
 rather than running a single docker compose file a single container for th both app.js & app test.js
 we can run both in a single file
+
+services:
+  web:
+    build:
+      context: .
+      dockerfile: Dockerfile.dev
+    ports:
+      - '3002:3000'
+    volumes:
+      - /app/node_modules
+      - .:/app
+
+  tests:
+    stdin_open: true
+    build: 
+      context: .
+      dockerfile: Dockerfile.dev
+    volumes:
+      - /app/node_modules
+      - .:/app
+    command: ["npm", "run", "test"]  
