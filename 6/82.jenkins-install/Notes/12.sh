ab7a60b7bed17bdc0f235da2629090c40dc1c5df
Rakesh <your-email@example.com>
2026-03-23 08:15:12 +0000

    adding code to docker K8s

--- 6/82.jenkins-install/Notes/12.sh
+++ 6/82.jenkins-install/Notes/12.sh
@@ -1,2 +1,27 @@
 Jan 28
+NOTES
+Groovy based Jenkins declarative pipeline
+(Jenkind Pipeline)
+- With the Jenkind pipeline file we can create the Jenkins pipeline along with that 
+
+JENKINSFILE SYNTAX
+
+its a declarative pipeline, Jenkins has its own keyword, it will sue the mopdel of Groovy,nbut the syntax are declarative .
+e.g 
+pipeline {
+
+}
+
+Jenkins using grrovy  mod, so when we wirht the sepcif word pipeline Jenkins will parse tthis before  running anything, if Jenkins found anything 
+wrong it will reject it.
+
+AGENT synbtax
+e.g 
+pipeline {
+agent any
+
+agent docker:dind
+}
+
+If we add as any it will run the pipeline on any available agent... Her jenkins will parse & pcik tthe nearest one agent
 
