2241dce2eba98eeebb1a2459a946b6aee226fcd3
Rakesh <your-email@example.com>
2026-04-10 15:38:40 +0000

    adding code to docker K8s

---
+++ 6/82.jenkins-install/Notes/14.sh
@@ -0,0 +1,11 @@
+31 Dec
+
+--Now the Jenkins build we are triggering , the build is accomplished in Built-in node where the My-Jenkins containeer the jenkins controller is
+also there
+---The agent where the builds are comminted now are done insde the built-in Node, which  can be found inside the jenkins GUI got Jenkins manage >>
+Nodes >> built-In-Node & that is the agent
+--Built in node is not a controller, its isntalled along with the controller but ot the controller it is, inside that all our prsent
+jenkins output is showing.
+-- The Built in node is in the My-Jenkins container
+-- Now we have used the agent as "Any" thats why its choosed the built-in-node agent or else if we add any other agents like Docker 
+    It will
