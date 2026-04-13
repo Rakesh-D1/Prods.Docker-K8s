d96a650611173bfbc25104107b36e8ed47d98312
Rakesh <your-email@example.com>
2026-04-13 10:38:15 +0000

    adding code to docker K8s

--- 6/82.jenkins-install/Notes/14.sh
+++ 6/82.jenkins-install/Notes/14.sh
@@ -9,3 +9,75 @@ jenkins output is showing.
 -- The Built in node is in the My-Jenkins container
 -- Now we have used the agent as "Any" thats why its choosed the built-in-node agent or else if we add any other agents like Docker 
     It will
+
+---------------------------------------------------------------------------------------------
+Now in the Jenkins build in last chapter we have added the function 
+    }
+    post {
+        always {
+            cleanWs()
+        }
+    }
+}
+at the post function its basically removed the artifcats after the build has generated whihc is actually a issue
+
+---
+
+Now to overcome this issue we need to first cleanup the workspace rather than in the POST , so the command is
+
+pipeline {
+    agent any
+
+    stages {
+        stage('Build-pc') {
+            steps {
+                cleanWs()
+                echo 'build a new pc'
+                sh 'mkdir -p Build-pc'
+                sh 'touch Build-pc/comp.txt'
+                sh 'echo "motherboard" >> Build-pc/comp.txt'
+                sh 'cat Build-pc/comp.txt'
+                sh 'echo "display" >> Build-pc/comp.txt'
+                sh 'cat Build-pc/comp.txt'
+                sh 'echo "keyboard" >> Build-pc/comp.txt'
+                sh 'cat Build-pc/comp.txt'
+            }
+        }
+    }
+
+
+Now in the POST block it will cehck the comp.txt  is present in the Build-pc directory
+    post {
+        success {
+          archieveArtificats artifacts: 'Build-pc/**'
+        }
+    }
+}
+
+
+Final this groovy file as worked
+pipeline {
+    agent any
+
+    stages {
+        stage('Build-pc') {
+            steps {
+                cleanWs()
+                echo 'build a new pc'
+                sh 'mkdir -p Build-pc'
+                sh 'touch Build-pc/comp.txt'
+                sh 'echo "motherboard" >> Build-pc/comp.txt'
+                sh 'cat Build-pc/comp.txt'
+                sh 'echo "display" >> Build-pc/comp.txt'
+                sh 'cat Build-pc/comp.txt'
+                sh 'echo "keyboard" >> Build-pc/comp.txt'
+                sh 'cat Build-pc/comp.txt'
+            }
+        }
+    }
+    post {
+    success {
+        archiveArtifacts artifacts: 'Build-pc/**'
+    }
+}
+}   
